f2aca9918ca73890f2e723c5c539b2072161e7db
Ian Crispi <cryptiquefl0ws@gmail.com>
2025-10-29 23:41:02 -0700

    Poems as Data

--- convex/poems.ts
+++ convex/poems.ts
@@ -3,6 +3,6 @@ import { query } from "./_generated/server";
 export const getPoems = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("poems").order("asc", "order").collect();
+    return await ctx.db.query("poems").order("asc").collect();
   },
 });
